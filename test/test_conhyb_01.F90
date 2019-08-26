5ad892450bf7a1dc5a61f4f63c0169fd6f1fa02e
chinoune <chinoune.mehdi@gmail.com>
2019-08-26 08:48:33 +0100

    Add testing.

---
+++ test/test_conhyb_01.F90
@@ -0,0 +1,16 @@
+program test_conhyp_01
+  use constants, only : RP
+  use conhyp_m, only : conhyp
+  implicit none
+  !
+  integer :: i
+  complex(RP) :: a, b, z
+  !
+  z = (0._RP,0._RP)
+  do i = 1, 200
+    a = cmplx( i*1._RP, 0._RP, RP )
+    b = cmplx( 0._RP, i*1._RP, RP )
+    if( conhyp( a, b, z )/=(1._RP,0._RP) ) ERROR STOP " Failed : 1_F_1(a,b,z=0)/=1"
+  end do
+  !
+end program test_conhyp_01
